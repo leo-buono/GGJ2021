1e93d965137828f7bd374f2c4f8bea51c378f959
Leo <leo.buono@ontariotechu.net>
2021-01-29 20:46:20 -0500

    Bless it up

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+
+    bool grabbed = false;
+    float distanceFromScreen = 6f;
+
+    public float distMax = 15f;
+    public float distMin = 5f;
+
+    private void Update()
+    {
+        if (grabbed)
+        {
+            //finding cursor position and then snapping the player to it
+            Ray mousePos = Camera.main.ScreenPointToRay(Input.mousePosition);
+            this.transform.position = mousePos.origin + (mousePos.direction * distanceFromScreen);
+            distanceFromScreen += Input.mouseScrollDelta.y;
+            if (distanceFromScreen < distMin)
+            {
+                distanceFromScreen = distMin;
+            }
+            else if (distanceFromScreen > distMax)
+            {
+                distanceFromScreen = distMax;
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        grabbed = true;
+    }
+    private void OnMouseUp()
+    {
+        grabbed = false;
+    }
+
+}
---
+++ Assets/Scripts/connectionZoneThing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class connectionZoneThing : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObj = collision.gameObject;
+        if (gameObj.layer == 8)
+        {
+
+        }
+    }
+}
